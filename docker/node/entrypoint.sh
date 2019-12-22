a47ae808ec8b4c89d83f0795be0833775adb79c5
Jakub Gabrys <jakub.gabrys@codibly.com>
2019-12-22 22:33:10 +0100

    Moved to yarn from npm

--- docker/node/entrypoint.sh
+++ docker/node/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+ng -v
 GREEN='\033[0;32m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
@@ -8,7 +9,7 @@ sudo chown -R node:node /usr/local/lib/node_modules
 sudo chown -R node:node /app/node_modules
 
 printf "${BLUE}Installing node_modules\n"
-npm install
+yarn install
 
 printf "${GREEN}Entrypoint finished\n"
 printf "${GREEN}Starting angular server\n${NC}"
